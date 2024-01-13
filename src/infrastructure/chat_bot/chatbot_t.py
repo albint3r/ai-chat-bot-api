4893dbdd73bf52aa48e7c4f56de6ef9e81cd1a4f
albint3r <albint3r@gmail.com>
2024-01-13 16:11:05 -0600

    Change Prompt Template

--- src/infrastructure/chat_bot/chatbot_t.py
+++ src/infrastructure/chat_bot/chatbot_t.py
@@ -1,12 +1,12 @@
 from typing import Any
 
-from langchain_community.vectorstores import Pinecone
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnablePassthrough, RunnableSerializable
+from langchain_core.runnables import RunnablePassthrough, RunnableSerializable, RunnableParallel
 from langchain_openai import ChatOpenAI
+
 from src.domain.chat_bot.entities.answer import Answer
 from src.domain.chat_bot.entities.question import Question
 from src.domain.chat_bot.repositories.i_vectors_repository import IVectorRepository
@@ -17,12 +17,14 @@ class ChatBotT(IChatBot):
     repo: IVectorRepository
     embeddings_model: Embeddings | None = None
     index_name: str
-    template: str = """Use the following pieces of context to answer the question at the end.
+    template: str = """You are an assistant chatbot that only will respond to questions about the professional experience 
+    of Alberto Ortiz Ascencio. Use the following pieces of context to answer the question at the end.
     If you don't know the answer, just say that you don't know, don't try to make up an answer.
     Use three sentences maximum and keep the answer as concise as possible.
-    Always say "thanks for asking!" at the end of the answer.
-
+    Only respond in the Question language.
+    <context>
     {context}
+    </context>
 
     Question: {question}
 
@@ -38,14 +40,12 @@ class ChatBotT(IChatBot):
         # Init Pinecone db
         llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
         self.repo.init()
-        # Todo: Add a class that wrap the vector store object:
         vectorstore = self.repo.get_vectorstore(self.index_name, self.embeddings_model, "text")
-        # vectorstore = Pinecone(index, self.embeddings_model, "text")
         # Get retrievers
         retriever = vectorstore.as_retriever()
         custom_prompt = PromptTemplate.from_template(self.template)
         return (
-                {"context": retriever | self._format_docs, "question": RunnablePassthrough()}
+                RunnableParallel(context=retriever | self._format_docs, question=RunnablePassthrough())
                 | custom_prompt
                 | llm
                 | StrOutputParser()
--- src/routes/chat_bot/chatbot.py
+++ src/routes/chat_bot/chatbot.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from langchain_openai import OpenAIEmbeddings
 
-from credentials_provider import credentials_provider
 from src.domain.chat_bot.entities.answer import Answer
 from src.domain.chat_bot.entities.question import Question
 from src.infrastructure.chat_bot.chatbot import ChatBotX
@@ -15,7 +14,7 @@ route = APIRouter(prefix='/chatbot',
 @route.post('/v1/qa-chatbot')
 def qa_chatbot(question: Question) -> Answer:
     chatbot = ChatBotX(index_name='tobecv', repo=PineconeRepository(),
-                       embeddings_model=OpenAIEmbeddings(openai_api_key=credentials_provider.open_ai_api_key))
+                       embeddings_model=OpenAIEmbeddings())
     answer = chatbot.query_question(question)
     return answer
 
@@ -23,6 +22,6 @@ def qa_chatbot(question: Question) -> Answer:
 @route.post('/v1/qa-chatbot2')
 def qa_chatbot2(question: Question) -> Answer:
     chatbot = ChatBotT(index_name='tobecv', repo=PineconeRepository(),
-                       embeddings_model=OpenAIEmbeddings(openai_api_key=credentials_provider.open_ai_api_key))
+                       embeddings_model=OpenAIEmbeddings())
     answer = chatbot.query_question(question)
     return answer
