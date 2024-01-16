01596bdd0e3741438fb33003c4446254dacc0a3d
albint3r <albint3r@gmail.com>
2024-01-16 16:13:31 -0600

    Add CRUD

--- credentials_provider.py
+++ credentials_provider.py
@@ -15,5 +15,17 @@ class _CredentialsProvider(BaseModel):
     def pinecone_ai_api_key(self) -> str:
         return self.env.get('PINECONE_API_KEY')
 
+    @property
+    def user(self) -> str:
+        return self.env.get('USER')
+
+    @property
+    def password(self) -> str:
+        return self.env.get('PASSWORD')
+
+    @property
+    def secret(self) -> str:
+        return self.env.get('SECRET')
+
 
 credentials_provider = _CredentialsProvider()
